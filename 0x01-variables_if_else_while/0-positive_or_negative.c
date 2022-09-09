f39eee536a0d8c2543ea88fa21dd7fc6547c5121
ntando007 <ntando.busakwe99@gmail.com>
2022-09-09 15:11:30 -0700

    fix8

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,12 +5,10 @@
 /* more headers goes there */
 
 
-
-/*
- * main - Entry point
- * Return: Always 0 (Success)
- *    */  /* betty style doc for function main goes there */
-
+/**                                                                
+ * main - Entry point                          
+ * Return: Always 0 (Success)                     
+ */ 
 int main(void)
 
 {
