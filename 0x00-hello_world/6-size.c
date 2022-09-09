6660ee850d5188d658c1b01410f6ffddedf8438a
ntando007 <ntando.busakwe99@gmail.com>
2022-09-08 19:31:45 -0700

    wjiyespace

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,13 +3,14 @@
  * main - Entry point
  * Return: Always 0 (Success)
  */
-int main(void) 
+int main(void)
 {
 	int Intype;
 	long int Longtype;
 	long long int Longlongtype;
 	char Chartype;
 	float Floattype;
+
 	printf("Size of a char: %zu byte(s)\n", sizeof(Chartype));
 	printf("Size of an int: %zu byte(s)\n", sizeof(Intype));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(Longtype));
