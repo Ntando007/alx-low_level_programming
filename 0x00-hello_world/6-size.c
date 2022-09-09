6bcf287ed18815d9cef5d223e2540ba71114f517
ntando007 <ntando.busakwe99@gmail.com>
2022-09-08 19:06:16 -0700

    fix7

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,6 +10,7 @@ int main(void)
 	long long int Longlongtype;
 	char Chartype;
 	float Floattype;
+	
 	printf("Size of a char: %zu byte(s)\n", sizeof(Chartype));
 	printf("Size of an int: %zu byte(s)\n", sizeof(Intype));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(Longtype));
