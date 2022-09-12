fd6934e98ecc5283797a69f7d0b7c5f6a49fd9ef
ntando007 <ntando.busakwe99@gmail.com>
2022-09-12 10:44:49 -0700

    test

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,21 @@
+/**
+ *
+ * main - Entry point / Comile code
+ * Description: print all aplhabet letters except q and e
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{	
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c != 'q' && c != 'e')
+		{
+			putchar(c);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
