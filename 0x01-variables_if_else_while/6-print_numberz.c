792678cece30742ee224b46c5e26d65f4e15b369
ntando007 <ntando.busakwe99@gmail.com>
2022-09-12 10:57:47 -0700

    digits

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,22 @@
+nclude <stdio.h>
+/**
+ *
+ * main - Compile code
+ *
+ * Description: Digits of base 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x ='0';x <= '9'; x++)
+	{
+		putchar(x);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
