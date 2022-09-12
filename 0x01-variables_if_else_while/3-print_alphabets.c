ef50cba7f05595966b7af8887a4c6ce12560a187
ntando007 <ntando.busakwe99@gmail.com>
2022-09-12 09:50:40 -0700

    script

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Compile code 
+ * Return : value always 0 for (Success)
+ */
+int main(void)
+	char c;
+	char x;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		for(x ='A'; x <= 'Z'; x++)
+		{	
+			putchar(c,x);
+	putchar('n');	
+		}
+	{
+	return (0);
