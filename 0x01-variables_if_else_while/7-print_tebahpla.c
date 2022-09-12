6bd436bbba9727828bdbe67265b3d899256196ef
ntando007 <ntando.busakwe99@gmail.com>
2022-09-12 11:03:02 -0700

    compile

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *
+ * main - Compile code
+ *
+ * Description: Prints the alphabet at reverse
+ * 
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c ='z'; c <= 'a'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+
+	return (0);
+}
