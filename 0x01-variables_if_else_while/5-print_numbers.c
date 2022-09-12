7c47999cd09268f07f44c15fbd58aa8a73b50e99
ntando007 <ntando.busakwe99@gmail.com>
2022-09-12 10:50:51 -0700

    single numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+/**
+ *
+ * main - Compile code
+ *
+ * Description : Print numbers since 0 to 9
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{	
+	char x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		putchar(x);
+	}
+
+	putchar('n');
+
+	return (0);
+}
