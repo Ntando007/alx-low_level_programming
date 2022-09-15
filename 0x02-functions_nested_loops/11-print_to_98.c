da047d7b2023263d43a3ccf2dbe1f7d6736b00c3
ntando007 <ntando.busakwe99@gmail.com>
2022-09-15 03:13:56 -0700

    compile fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 /**
  * main - compile code
- * void print_to_98(int n) - showing natural numbers
+ * void print_to_98(int ) - showing natural numbers
+ * n - number to start from
  * Return: Always 0
  */
 void print_to_98(int n)
