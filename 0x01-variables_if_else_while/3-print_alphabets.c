d260213be4674668fbd83ffee4dc69de8a817508
ntando007 <ntando.busakwe99@gmail.com>
2022-09-17 06:02:14 -0700

    compile fix

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 /**
- * main - Compile code 
+ * main - Compile code
  * Return : value always 0 for (Success)
  */
 int main(void)
 {
 	char c;
 	char x;
-	
 	for (c = 'a'; c <= 'z'; c++)
 	{
 		putchar(c);
 	}
-	for(x ='A'; x <= 'Z'; x++)
+	for (x ='A'; x <= 'Z'; x++)
 	{
 		putchar(x);
 	}
