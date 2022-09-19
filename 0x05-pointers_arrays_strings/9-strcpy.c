921b5b3992f12294f16cfddebe4fbb5c2c958d0d
ntando007 <ntando.busakwe99@gmail.com>
2022-09-19 15:55:59 -0700

    task

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strcpy - copy string to src
+ * @dest - value
+ * @src - value
+ * Return: Success
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+	int l;
+
+	for (l = 0; src[l] !- '\0'; l++)
+	{
+	}
+
+	for (l = 0; i <= l; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
