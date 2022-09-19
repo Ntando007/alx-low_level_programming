2aaece1cea0626d3ab85c5d32dfa31f0e5f00d2b
ntando007 <ntando.busakwe99@gmail.com>
2022-09-19 13:35:45 -0700

    swap values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * swap_int(int*a, int *b) - swaps values of intergers.
+ * @a - takes value and swap to b
+ * @b - takes value and swap to a
+ * Return: Success
+ */
+void swap_int(int *a, int *b)
+{
+	int c;
+	
+	c = *a;
+	*a = *b;
+	*b = c;
+}
+	
