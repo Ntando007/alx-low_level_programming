b01c94c90ef65fe6d26912ad93eb898fb3f27144
ntando007 <ntando.busakwe99@gmail.com>
2022-09-19 16:28:02 -0700

    style fix

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,7 @@
 #include "main.h"
 /**
- * reset_to_98(int *n) - takes pointer and updates value
- *
- *
+ * reset_to_98(int *n) - takes pointer *n  and updates value
  * @n = value string
- * Return: Success
  */
 void reset_to_98(int *n)
 {
