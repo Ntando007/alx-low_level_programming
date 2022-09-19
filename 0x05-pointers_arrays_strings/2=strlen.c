08de52cf24c1aaf6c5b0a7c99395c7b34ced09fa
ntando007 <ntando.busakwe99@gmail.com>
2022-09-19 13:51:39 -0700

    showcasting string length

---
+++ 0x05-pointers_arrays_strings/2=strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * int _strlen(char *s) - returns length of string
+ * @s - char string 
+ * Return: Always (Success)
+ */
+int strlen(char *s)
+{
+	char *s;
+
+	s = i;
+	i = _strlen(s);
+	printf("%d\n", i);
+	return (0);
+}
