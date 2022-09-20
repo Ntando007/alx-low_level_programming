c5e735fcc9c567770f37fa592b94f8a61fae81f3
ntando007 <ntando.busakwe99@gmail.com>
2022-09-19 17:30:45 -0700

    style change

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * _puts(char *str) - show string and puts to stdout
- * @str - string value
+ * _puts - show string and puts to stdout
+ * @str: string value
  * Return: Success
  */
 void _puts(char *str)
@@ -10,7 +10,7 @@ void _puts(char *str)
 
 	for (c = 0; str[c] != '\0'; c++)
 	{
-		_putchar (str[c]);
+		_putchar(str[c]);
 	}
 	_putchar('\n');
 }
--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,7 +11,10 @@ void puts2(char *str)
 	for (c = 0; str[c] != '\0'; c++)
 	{
 		if (c % 2 == 0)
-			_putchar (str[c]);
+		{
+
+			_putchar(str[c]);
+		}
 	}
 
 	_putchar ('\n');
