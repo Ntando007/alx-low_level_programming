64c867f719351f33f70f902b0388cdcd9cd1b8de
ntando007 <ntando.busakwe99@gmail.com>
2022-09-21 12:49:04 -0700

    code fix

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
- * _mul(int a, int b) - multiplies two integers
- * @a - int multiplied by b
- * @b - int multiplied  by a
+ * _mul - multiplies two integers
+ * @a: int multiplied by b
+ * @b: int multiplied  by a
  * Return: Always 0
  */
 int mul(int a, int b)
 {
 	int c;
 	c = a * b;
-	
+
 	return(c);
 }
