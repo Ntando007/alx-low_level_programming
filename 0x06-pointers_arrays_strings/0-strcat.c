81f0b608f11d3f8042559f05612402868ed1ef3a
ntando007 <ntando.busakwe99@gmail.com>
2022-09-21 04:44:43 -0700

    string concatenate

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _strcat - concatebates two string
+ * @dest: character value
+ * @src: character value
+ * Return: Success
+ */
+char *_strcat(char *dest, char *src);
+{
+	char src[] = "Hello ";
+	char dest[] = "World!";
+	strcat(dest, source);
+	putchar(src, dest);
+
+	return (0);
+}
