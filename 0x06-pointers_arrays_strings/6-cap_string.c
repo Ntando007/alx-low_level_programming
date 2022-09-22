0d3abfc9386447bc0a5536ddab6ba06e5e56a465
ntando007 <111117959+Ntando007@users.noreply.github.com>
2022-09-22 23:57:02 +0200

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,11 +1,12 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * cap_string - Capitalizes all words of a string.
- * @str: The string to be capitalized.
+ * cap_string - Capitalizes words
+ * @str: string to be capitalized.
  *
  * Return: A pointer to the changed string.
  */
+
 char *cap_string(char *str)
 {
 	int i = 0;
