157d9b664dedb205d14ff548d55a09344bd6aaba
ntando007 <ntando.busakwe99@gmail.com>
2022-09-27 00:36:36 -0700

    push file

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"/**
+/**
+ * _memset(char *s, char b, unsigned int n) - fill memory with a constant byte
+ * @n: bytes of the memory area
+ * @s: pointed too
+ * @b: constant byte b
+ *
+ * Return: memory area
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i; //declare var
+
+	for (i = 0; n > 0; i++, n--)
+	{
+		s[i] = b; //access i and constat to b
+	}	
+	
+	return (s); //pointer to memory area s}
