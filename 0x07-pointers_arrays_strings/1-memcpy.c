ffbec2822c162f65b1578c12bc02d1fca12e457d
ntando007 <ntando.busakwe99@gmail.com>
2022-09-27 02:04:12 -0700

    file add

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memcpy - copies memory area
+ * @n: bytes
+ * @src: memory area
+ * @dest: memory area
+ *
+ * Return: dest pointer
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	usigned int i;
+
+	for (i = 0; n > 0; i++, n--)
+	{
+		src[i] = dest;
+	}
+
+	return (dest);
+}
