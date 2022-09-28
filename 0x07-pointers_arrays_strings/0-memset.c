ffac2e154ac033046fd96128b68ff3b17f1cc3c7
ntando007 <111117959+Ntando007@users.noreply.github.com>
2022-09-28 05:34:46 +0200

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -10,12 +10,12 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i; //declare var
+	unsigned int i; /*declare var*/
 
 	for (i = 0; n > 0; i++, n--)
 	{
-		s[i] = b; //access i and constat to b
-	}	
-	
-	return (s); //pointer to memory area s
+		s[i] = b; /*access i and constat to b*/
+	}
+
+	return (s); /*pointer to memory area s*/
 }
