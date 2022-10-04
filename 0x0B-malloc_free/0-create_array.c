275da8cdff88baddda8bd27884fa3d695dfc7049
ntando007 <ntando.busakwe99@gmail.com>
2022-10-04 08:34:39 -0700

    work

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * create_array - creates array of char
+ * @size: size of memory
+ * @c: character
+ *
+ * Return: NULL
+ */
+*create_array(unsigned int size, char c)
+{
+	unsigned int s;
+	int n;
+
+	for(s = 0; s<n; s+++)
+		A
+
+	size = 0;
+	
