3b9b29908a6b0fb09e78c44596c7ea229265fa9e
ntando007 <111117959+Ntando007@users.noreply.github.com>
2022-10-05 11:54:39 +0200

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -11,6 +11,7 @@
 char *create_array(unsigned int size, char c)
 {
 	unsigned int s;
+	int i;
 	/*char c;*/
 
 	if (size <= 0)
@@ -24,7 +25,7 @@ char *create_array(unsigned int size, char c)
 	for (i = 0; i < size; i++)
 		*(s +1) = c;
 
-	*(s + i) = '\0';
+	(s + i) = '\0';
 
 	return (s);
 }	
