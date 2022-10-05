511d20636e572c917f510dd8216e9e58727f0d5c
ntando007 <111117959+Ntando007@users.noreply.github.com>
2022-10-05 22:08:38 +0200

    execution

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ *@argc: counts
+ *@argv: pointer to array
+ *Return: Success
+ */
+int main(int argc, char *argv[])
+{
+  if (argc > 1)
+    printf("%s\n", argv[0]);
+  return (0);
+}
