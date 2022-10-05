08b821d073dd9eb9089f6bd79de71932348cbeb7
ntando007 <ntando.busakwe99@gmail.com>
2022-10-05 13:24:59 -0700

    file fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 
 /**
-<<<<<<< HEAD
  * main - entry point
  *
  * @argc: counts
@@ -15,18 +14,4 @@ int main(int argc, char *argv[])
 	if (argc > 0)
 		printf("%s\n", argv[0]);
 	return (0);
-=======
- * main - Entry point
- *
- *@argc: counts
- *@argv: pointer to array
- *Return: Success
- */
- 
-int main(int argc, char *argv[])
-{
-  if (argc > 0)
-    printf("%s\n", argv[0]);
-  return (0);
->>>>>>> 6e87a312e64db8ea5c707853b088e8df8fb2e624
 }
