7ee48a83c9d70e2ee12a3a0afdd82b30b7708cd8
ntando007 <111117959+Ntando007@users.noreply.github.com>
2022-10-05 22:37:19 +0200

    file1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ *
+ *@argc: counts
+ *@argv: Pointer to array
+ *Return: Success
+ */
+
+int main(int argc, char **argv)
+{
+	(void) argv;
+	printf("%i\n", argc - 1);
+	return (0);
+}
+ 
+ 
