552a5dacddd5a197aca1b1a14db0bb84d9977cf6
ntando007 <ntando.busakwe99@gmail.com>
2022-10-10 02:36:15 -0700

    file add

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * struct dog - check code
+ *
+ * @name: character
+ * @age: float
+ * @owner: character
+ *
+ * Return: Success
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
