7c67a501ff62c31dafdee4567e4a6e062742110a
ntando007 <111117959+Ntando007@users.noreply.github.com>
2022-10-11 01:31:18 +0200

    update

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -12,16 +12,16 @@ char *_copy(char *src)
   char *p;
   int x;
   int y;
-  
+
   if (src == NULL)
   {
     return (NULL);
   }
-  
+
   for (y = 0; src[y] != '\0'; y++)
     ;
   p = malloc(sizeof(char) * (y + 1));
-  
+
   if (p == NULL)
 	{
 		return (NULL);
