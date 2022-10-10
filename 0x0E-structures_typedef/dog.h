4e67d2640a494a163724b267c4db4a064bae3612
ntando007 <111117959+Ntando007@users.noreply.github.com>
2022-10-11 00:01:16 +0200

    changes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,6 @@
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
+
 /**
  * struct dog - check code
  *
@@ -8,9 +10,18 @@
  *
  * Return: Success
  */
-struct dog_t
+ 
+struct dog
 {
 	char *name;
 	char *owner;
 	float age;
 };
+
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif /*DOG_H*/
