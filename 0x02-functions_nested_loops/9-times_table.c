9c7100ee39224886183cb32c48d90016bdd47939
ntando007 <111117959+Ntando007@users.noreply.github.com>
2022-10-20 22:14:51 +0200

    update file

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,47 +9,34 @@ void times_table(void)
 	int b;
 	int c;
 
-	for (a = 0; a <= 9; a++)
-	{
-
-		for (b = 0; b <= 9; b++)
-		{
-			c = a * b;
-
-			if ((c / 10) == 0)
-			{
-				if (b == 0)
-				{
-					_putchar ('0');
-
-				}
-
-				if (b != 0)
-				{
-					_putchar (' ');
-					_putchar ((c % 10) + '0');
-
-				}
-
-				if (b < 9)
-
-				{
-					_putchar(',');
-					_putchar (' ');
-				}
-
-			}
-			else
-			{
-				_putchar ((c / 10) + '0');
-				_putchar ((c % 10) + '0');
-
-				if (b < 9)
-				{
-					_putchar(',');				_putchar (' ');
-				}
-			}
-		}
-		_putchar ('\n');
-	}
+while (a < 10)
+b = 0;
+while (b < 10)
+{
+c = a * b;
+if (c > 9)
+{
+_putchar(c / 10 + '0');
+_putchar(c % 10 + '0');
 }
+else if (b != 0)
+{
+_putchar(' ');
+_putchar(c + '0');
+}
+else
+{
+_putchar(c + '0');
+}
+if (b != 9)
+{
+_putchar(',');
+_putchar(' ');
+}
+b++;
+}
+_putchar('\n');
+a++;
+}
+}
+
