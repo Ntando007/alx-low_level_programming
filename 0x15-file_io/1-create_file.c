c444d9708cbd33cf209d1c8188cd602261f394c6
ntando007 <ntando.busakwe99@gmail.com>
2022-11-03 05:15:49 -0700

    update file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,10 +22,10 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	
+
 	if (text_content == NULL)
 	{
-		text_content ="";
+		text_content = "";
 	}
 
 	for (x = 0; text_content[x]; x++)
@@ -35,7 +35,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	close(k);
+	close(kk);
 
 	return (1);
 }
