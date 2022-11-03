948fbff4af80e01e23619c70c9a8cb118645baff
ntando007 <111117959+Ntando007@users.noreply.github.com>
2022-11-03 18:09:35 +0200

    file edit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,10 +9,8 @@ void print_most_numbers(void)
 
 	for (x = '0'; x <= '9'; x++)
 	{
-		if ((x != '2') && (x != '4'))
-		
-		_putchar (x);
-		
-	_putchar ('\n');
+	if ((x != '2') && (x != '4'))	
+	_putchar (x);
 	}
+	_putchar ('\n');
 }
