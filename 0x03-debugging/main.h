a061b3bac666fcd1551a98b64f8e487a9ebdb893
ntando007 <111117959+Ntando007@users.noreply.github.com>
2022-11-03 23:25:03 +0200

    changes

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,5 +1,5 @@
-#ifndef main.h
-#define main.h
+#ifndef MAIN_H
+#define MAIN_H
 void positive_or_negative(int n);
 int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
