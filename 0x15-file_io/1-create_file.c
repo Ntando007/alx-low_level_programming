d635e6f895796b58004ecedc7241676fc457faef
ntando007 <ntando.busakwe99@gmail.com>
2022-11-03 05:10:17 -0700

    file fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,27 +5,37 @@
  * @text_content: empty file
  * Return: 1 (Success) / -1 (NULL)
  */
-int create_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content)
 {
-	char k;
+	int k;
+	int x;
+	int r;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
 
-	k = touch text_content
+	k = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	if (k == -1)
 	{
-		return (1);
+		return (-1);
 	}
-	else
+	
+	if (text_content == NULL)
 	{
-		return (0);
+		text_content ="";
 	}
 
-	text_content = k;
+	for (x = 0; text_content[x]; x++)
+		;
+	r = write(fd, text_content, x);
+	if (r == -1)
+	{
+		return (-1);
+	}
+	close(fd);
 
-	return (k);
+	return (1);
 }
