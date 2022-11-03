69f272ffd262580cd5e24f706bc6eb7d5dfc3d06
ntando007 <ntando.busakwe99@gmail.com>
2022-11-03 05:52:30 -0700

    updated file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * append_text_to_file - appends text to end of file
- * @filename: file name 
+ * @filename: file name
  * @text_content: empty file
  * Return: 1 (Succes) / -1 (NULL)
  */
