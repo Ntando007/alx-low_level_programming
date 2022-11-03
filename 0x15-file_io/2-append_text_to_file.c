1c946f3c3de1db0ae66d2f841ef01d04f6f0c515
ntando007 <ntando.busakwe99@gmail.com>
2022-11-03 05:27:36 -0700

    file fix

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,21 +1,22 @@
 #include "main.h"
 /**
  * append_text_to_file - appends text to end of file 
- * @filename: file name
- * @text_content: empty file
+ * @filename: file name * @text_content: empty file
  * Return: 1 (Succes) / -1 (NULL)
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int ap;
 	int r;
-	int w;
 
 
-	if (ap == NULL)
+	if (text_content == NULL)
 	{
-		text_content;
+		text_content = "";
 	}
+
+	for (r = 0; text_content[r] != '\0'; r++)
+		;
 
 	/* OPEN FILE */
 
