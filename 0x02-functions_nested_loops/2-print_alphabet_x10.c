5f7a258a51e80533b239c8c0da89a1fbc57bd5e5
ntando007 <111117959+Ntando007@users.noreply.github.com>
2022-11-07 13:00:22 +0200

    file fix2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,12 +12,11 @@ void print_alphabet_x10(void)
 	counts = 0;
 	while (counts < 10)
 	{
-		for (letter = 'a'; letter <= 'z'; letter++){
-
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
 			_putchar(letter);
 			letter++;
 		}
+		_putchar('\n');
 	}
-
-	_putchar('\n');
 }
