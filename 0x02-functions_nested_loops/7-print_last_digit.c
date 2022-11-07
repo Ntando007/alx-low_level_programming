15a85bcf094d0811dceef73d0832cf5fc5dc2c1b
ntando007 <111117959+Ntando007@users.noreply.github.com>
2022-11-07 13:38:10 +0200

    file fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "main.h"
-/** 
+/**
  * print_last_digit - show last number
  * Return: Always 0
  */
-int print_last_digit(int n)
+int print_last_digit(int)
 {
 int x;
 if (n < 0)
