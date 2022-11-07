922544ffb56024a4db30a9f576f9198b7b4f9dc2
ntando007 <111117959+Ntando007@users.noreply.github.com>
2022-11-07 12:48:02 +0200

    file fix1

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * main - compile and check code
- * int print_alphabet_x10(void) - show alphabets
+ * print_alphabet_x10- show alphabets
  * Return: Always 0
  */
 void print_alphabet_x10(void)
