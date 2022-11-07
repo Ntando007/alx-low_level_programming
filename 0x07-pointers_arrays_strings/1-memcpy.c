0644629b028529d6816a105029c951144b320dcf
ntando007 <111117959+Ntando007@users.noreply.github.com>
2022-11-07 22:04:13 +0200

    update file

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,9 +12,9 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; n > 0; i++, n--)
+	for (i = 0; n > 0; i++)
 	{
-		src[i] = dest;
+		src[i] = dest[i];
 	}
 
 	return (dest);
